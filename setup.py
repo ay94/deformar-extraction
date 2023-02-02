cd6ffad6a2d90f988f550c31f3e9dbcc54a891e6
ay94 <ahmed.hamadino1994@gmail.com>
2023-02-02 11:35:53 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,7 @@
 from setuptools import setup, find_packages
+
+setup(
+    name='experiment_utils',
+    version='0.1.0',
+    packages=find_packages(include=['experiment_utils', 'experiment_utils.*'])
+)
