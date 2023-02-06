6a2445b0e5294f32a8c93a3b831317b0bc34b8f1
ay94 <ahmed.hamadino1994@gmail.com>
2023-02-06 20:32:37 +0000

    FineTuneConfig to set experiment config

---
+++ experiment_utils/finetune_utils.py
@@ -0,0 +1,11 @@
+class FineTuneConfig:
+  def __init__(self) -> None:
+      self.MAX_SEQ_LEN = 256
+      self.TRAIN_BATCH_SIZE = 16
+      self.VALID_BATCH_SIZE = 8
+      self.EPOCHS = 4
+      self.SPLITS = 4
+      self.LEARNING_RATE = 5e-5
+      self.WARMUP_RATIO = 0.1
+      self.MAX_GRAD_NORM = 1.0
+      self.ACCUMULATION_STEPS = 1
