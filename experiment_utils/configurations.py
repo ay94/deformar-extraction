9e1fa214cf5bdd05ef9717879ecf83c123263f8c
ay94 <ahmed.younes.sam@gmail.com>
2024-07-20 07:14:15 +0100

    configurations

---
+++ experiment_utils/configurations.py
@@ -0,0 +1,124 @@
+import logging
+from dataclasses import dataclass
+from typing import Optional, Any, Dict
+
+@dataclass
+class TrainingConfig:
+    train_batch_size: int
+    test_batch_size: int
+    shuffle: bool
+    epochs: int
+    splits: int
+    learning_rate: float
+    warmup_ratio: float
+    max_grad_norm: float
+    accumulation_steps: int
+    logging_step: int
+
+    def __post_init__(self):
+        self.validate_config()
+
+    @staticmethod
+    def from_dict(config_dict: Dict[str, Any]):
+        try:
+            config_dict['learning_rate'] = float(config_dict['learning_rate'])
+        except ValueError as e:
+            logging.error("Invalid format for learning_rate, needs to be convertible to float")
+            raise ValueError("Invalid format for learning_rate") from e
+        return TrainingConfig(**config_dict)
+
+    def validate_config(self):
+        if not (0 < self.learning_rate < 1):
+            logging.error("Invalid learning rate: %s", self.learning_rate)
+            raise ValueError("Learning rate must be between 0 and 1")
+        if not (0 < self.warmup_ratio < 1):
+            logging.error("Invalid warmup ratio: %s", self.warmup_ratio)
+            raise ValueError("Warmup ratio must be between 0 and 1")
+        if self.epochs <= 0:
+            logging.error("Invalid number of epochs: %s", self.epochs)
+            raise ValueError("Epochs must be greater than 0")
+        if self.train_batch_size <= 0 or self.test_batch_size <= 0:
+            logging.error("Invalid batch sizes: Train %s, Valid %s", self.train_batch_size, self.test_batch_size)
+            raise ValueError("Batch sizes must be greater than 0")
+        if self.accumulation_steps < 1:
+            logging.error("Invalid accumulation steps: %s", self.accumulation_steps)
+            raise ValueError("Accumulation steps must be at least 1")
+        if self.logging_step < 1:
+            logging.error("Invalid logging steps: %s", self.logging_step)
+            raise ValueError("Accumulation steps must be at least 1")
+        logging.info("Configuration validated successfully")
+
+
+
+@dataclass
+class TokenizationStrategy:
+    type: str
+    index: int
+    schema: Optional[str] = None
+
+@dataclass
+class TokenizationConfig:
+    tokenizer_path: str
+    preprocessor_path: str
+    max_seq_len: int
+    strategy: TokenizationStrategy
+
+    def __post_init__(self):
+        self.validate_config()
+
+    @staticmethod
+    def from_dict(config_dict: Dict[str, Any]):
+        # This assumes the 'strategy' sub-dictionary is properly formatted
+        strategy_config = TokenizationStrategy(**config_dict['strategy'])
+        return TokenizationConfig(
+            tokenizer_path=config_dict['tokenizer_path'],
+            preprocessor_path=config_dict['preprocessor_path'],
+            max_seq_len=config_dict['max_seq_len'],
+            strategy=strategy_config
+        )
+
+    def validate_config(self):
+        if not isinstance(self.max_seq_len, int) or self.max_seq_len <= 0:
+            raise ValueError("max_seq_len must be a positive integer")
+        if not self.tokenizer_path:
+            raise ValueError("tokenizer_path cannot be empty")
+        if not self.preprocessor_path:
+            raise ValueError("preprocessor_path cannot be empty")
+        if not isinstance(self.strategy, TokenizationStrategy):
+            raise ValueError("strategy must be an instance of TokenizationStrategy")
+        if self.strategy.type not in ['core', 'all']:  # Example check
+            raise ValueError("Invalid strategy type specified")
+        if not isinstance(self.strategy.index, int) or self.strategy.index < 0:
+            raise ValueError("Strategy index must be a non-negative integer")
+
+
+
+@dataclass
+class ModelConfig:
+    model_path: str
+    dropout_rate: float
+    enable_attentions: bool
+    enable_hidden_states: bool
+    initialize_output_layer: bool
+
+    def __post_init__(self):
+        self.validate_config()
+
+    @staticmethod
+    def from_dict(config_dict):
+        return ModelConfig(**config_dict)
+
+    def validate_config(self):
+        if not self.model_path:
+            raise ValueError("Model path cannot be empty")
+        if not (0 <= self.dropout_rate <= 1):
+            raise ValueError("Dropout rate must be between 0 and 1")
+        if not isinstance(self.enable_attentions, bool):
+            raise ValueError("enable_attentions must be a boolean value")
+        if not isinstance(self.enable_hidden_states, bool):
+            raise ValueError("enable_hidden_states must be a boolean value")
+        if not isinstance(self.initialize_output_layer, bool):
+            raise ValueError("initialize_output_layer must be a boolean value")
+
+
+
