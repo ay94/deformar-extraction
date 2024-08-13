421b046c659b45652068470bfbd2f82aa984ff18
ay49 <ahmed.younes.sam@gmail.com>
2024-08-13 15:41:57 +0100

    bug fix

--- experiment_utils/config_managers.py
+++ experiment_utils/config_managers.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Any, Dict, List, Optional
 
-from experiment_utils.utisl import FileHandler
+from experiment_utils.utils import FileHandler
 
 
 @dataclass
